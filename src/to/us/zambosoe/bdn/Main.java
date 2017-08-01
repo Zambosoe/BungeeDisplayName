13d42764b00bdd7dccc629d6fd14278259b3b3ae
Zambosoe <Zambosoe@hotmail.com>
2017-08-01 07:31:53 -0400

    Testing begins
    Starting a new config.

--- src/to/us/zambosoe/bdn/Main.java
+++ src/to/us/zambosoe/bdn/Main.java
@@ -1,8 +1,6 @@
 package to.us.zambosoe.bdn;
 
-import com.google.common.io.ByteStreams;
 import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.event.ServerConnectEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
@@ -11,15 +9,15 @@ import net.md_5.bungee.config.ConfigurationProvider;
 import net.md_5.bungee.config.YamlConfiguration;
 import net.md_5.bungee.event.EventHandler;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main
-        extends Plugin
-        implements Listener
+public class Main extends Plugin implements Listener
 {
-    public Configuration c;
+    //Global Variables
+    Configuration configuration = null;
     public File f;
     public String pluginTag;
     public List<String> servers = new ArrayList();
@@ -27,6 +25,7 @@ public class Main
     public boolean countColors;
     public boolean countSpaces;
 
+    //Start Plugin
     public void onEnable()
     {
         getLogger().info("");
@@ -46,164 +45,44 @@ public class Main
         getProxy().getPluginManager().registerCommand(this, new Nickname(this));
         getProxy().getPluginManager().registerListener(this, this);
 
-        cc();
-        lc();
-        sc();
+        //Get Config
+        Load_Config();
     }
 
-    public void cc()
-    {
-        if (!getDataFolder().exists()) {
-            getDataFolder().mkdir();
-        }
-        this.f = new File(getDataFolder(), "config.yml");
-        if (!this.f.exists()) {
-            try
-            {
-                this.f.createNewFile();
-                InputStream is = getResourceAsStream("config.yml");Throwable localThrowable6 = null;
-                try
-                {
-                    OutputStream os = new FileOutputStream(this.f);Throwable localThrowable7 = null;
-                    try
-                    {
-                        ByteStreams.copy(is, os);
-                    }
-                    catch (Throwable localThrowable1)
-                    {
-                        localThrowable7 = localThrowable1;throw localThrowable1;
-                    }
-                    finally {}
-                }
-                catch (Throwable localThrowable4)
-                {
-                    localThrowable6 = localThrowable4;throw localThrowable4;
-                }
-                finally
-                {
-                    if (is != null) {
-                        if (localThrowable6 != null) {
-                            try
-                            {
-                                is.close();
-                            }
-                            catch (Throwable localThrowable5)
-                            {
-                                localThrowable6.addSuppressed(localThrowable5);
-                            }
-                        } else {
-                            is.close();
-                        }
-                    }
-                }
-            }
-            catch (IOException e)
-            {
-                throw new RuntimeException("Unable to create configuration file", e);
-            }
+    //Load Config
+    void Load_Config(){
+        try {
+            Configuration configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(new File(getDataFolder(), "config.yml"));
+        } catch (IOException e) {
+            getLogger().severe("Error: Could not load the config, did you remove it?");
         }
     }
 
-    public void lc()
-    {
-        this.servers.clear();
-        try
-        {
-            this.c = ConfigurationProvider.getProvider(YamlConfiguration.class).load(this.f);
-        }
-        catch (IOException e)
-        {
-            e.getCause();
-        }
-        String pt;
-        if (this.c.get("Plugin.Plugin_Tag") != null)
-        {
-            pt = this.c.getString("Plugin.Plugin_Tag");
-            this.pluginTag = ChatColor.translateAlternateColorCodes('&', pt);
-        }
-        else
-        {
-            this.c.set("Plugin.Plugin_Tag", "&f&l[&9BDN&f&l]&9 ");
-            this.pluginTag = ChatColor.translateAlternateColorCodes('&', this.c.getString("Plugin.Plugin_Tag"));
-        }
-        if (this.c.get("Plugin.Allowed_Servers") != null)
-        {
-            for (String s : this.c.getStringList("Plugin.Allowed_Servers")) {
-                this.servers.add(s.toLowerCase());
-            }
-        }
-        else
-        {
-            this.servers.add("example");
-            this.servers.add("server");
-            this.servers.add("names");
-            this.c.set("Plugin.Allowed_Servers", this.servers);
-        }
-        if (this.c.get("Plugin.Limit_Name_Length.Count_Spaces") != null)
-        {
-            this.countSpaces = this.c.getBoolean("Plugin.Limit_Name_Length.Count_Spaces");
-        }
-        else
-        {
-            this.c.set("Plugin.Limit_Name_Length.Count_Spaces", Boolean.valueOf(true));
-            this.countSpaces = this.c.getBoolean("Plugin.Limit_Name_Length.Count_Spaces");
-        }
-        if (this.c.get("Plugin.Limit_Name_Length.Count_&") != null)
-        {
-            this.countColors = this.c.getBoolean("Plugin.Limit_Name_Length.Count_&");
-        }
-        else
-        {
-            this.c.set("Plugin.Limit_Name_Length.Count_&", Boolean.valueOf(false));
-            this.countColors = this.c.getBoolean("Plugin.Limit_Name_Length.Count_&");
-        }
-        if (this.c.get("Plugin.Limit_Name_Length.Max") != null)
-        {
-            this.limit = this.c.getInt("Plugin.Limit_Name_Length.Max");
-        }
-        else
-        {
-            this.c.set("Plugin.Limit_Name_Length.Max", "25");
-            this.limit = this.c.getInt("Plugin.Limit_Name_Length.Max");
+    //Save Config
+    void Save_Config(){
+        try {
+            ConfigurationProvider.getProvider(YamlConfiguration.class).save(configuration, new File(getDataFolder(), "config.yml"));
+        } catch (IOException e) {
+            getLogger().severe("Error: Could not save the config, did you remove it?");
         }
     }
 
-    public void sc()
-    {
-        try
-        {
-            ConfigurationProvider.getProvider(YamlConfiguration.class).save(this.c, new File(getDataFolder(), "config.yml"));
-        }
-        catch (IOException e)
-        {
-            e.getCause();
-        }
-    }
 
+    //Connection
     @EventHandler
-    public void beforconnect(ServerConnectEvent e)
+    public void Before_Connection(ServerConnectEvent e)
     {
-        lc();
         if (this.servers.contains(e.getTarget().getName().toLowerCase())) {
-            if (this.c.get(e.getPlayer().getUniqueId().toString()) != null)
+            if (configuration.get(e.getPlayer().getUniqueId().toString()) != null)
             {
-                String s = this.c.getString(e.getPlayer().getUniqueId().toString());
+                String s = configuration.getString(e.getPlayer().getUniqueId().toString());
                 String co = ChatColor.translateAlternateColorCodes('&', s);
                 e.getPlayer().setDisplayName(co);
             }
             else
             {
-                this.c.set(e.getPlayer().getUniqueId().toString(), e.getPlayer().getDisplayName());
+                configuration.set(e.getPlayer().getUniqueId().toString(), e.getPlayer().getDisplayName());
             }
         }
-        sc();
-    }
-
-    @EventHandler
-    public void postloginevent(PostLoginEvent e) {}
-
-    public void onDisable()
-    {
-        getLogger().info("Disabled.");
     }
 }
--- src/to/us/zambosoe/bdn/Realname.java
+++ src/to/us/zambosoe/bdn/Realname.java
@@ -18,45 +18,6 @@ public class Realname
 
     public void execute(CommandSender commandSender, String[] strings)
     {
-        if ((commandSender instanceof ProxiedPlayer))
-        {
-            ProxiedPlayer player = (ProxiedPlayer)commandSender;
-            if (this.main.servers.contains(player.getServer().getInfo().getName().toLowerCase())) {
-                if (player.hasPermission("bdn.realname"))
-                {
-                    if (strings.length == 0)
-                    {
-                        player.sendMessage(this.main.pluginTag + ChatColor.RED + "/Whois <name>");
-                    }
-                    else
-                    {
-                        if (strings.length == 1)
-                        {
-                            ProxiedPlayer target = null;
-                            for (ProxiedPlayer p : this.main.getProxy().getPlayers()) {
-                                if ((p.getDisplayName().equalsIgnoreCase(strings[0])) || (p.getName().equalsIgnoreCase(strings[0]))) {
-                                    target = p;
-                                }
-                            }
-                            if (target != null)
-                            {
-                                player.sendMessage(this.main.pluginTag + ChatColor.GREEN + "Real name: " + ChatColor.RESET + target.getName());
-                                player.sendMessage(this.main.pluginTag + ChatColor.GREEN + "Display name: " + ChatColor.RESET + target.getDisplayName());
-                            }
-                            else
-                            {
-                                player.sendMessage(this.main.pluginTag + ChatColor.RED + "Player: " + ChatColor.RESET + strings[0] + ChatColor.RED + " not found.");
-                            }
-                        }
-                        if (strings.length > 1) {
-                            player.sendMessage(this.main.pluginTag + ChatColor.RED + "To many things in command.");
-                        }
-                    }
-                }
-                else {
-                    player.sendMessage(this.main.pluginTag + ChatColor.RED + "Sorry, you don't have permission for this.");
-                }
-            }
-        }
+        //Disabled for testing.
     }
 }
